926576c01ab390f5697ca0a5044e2110db802086
andrea-herace <82849639+andrea-herace@users.noreply.github.com>
2021-04-24 20:42:51 -0500

    Add files via upload

---
+++ area de un triangulo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main ()
+{
+    /* Andrea Hernández 
+    Equipo
+    22 abr 21
+    Programa: Areas de triangulo 
+    */
+}
+
+int main()
+{
+        float altura, base, area;
+
+        printf ("\n\n Introduzca base real");
+        scanf ("%f", &base);
+        printf ("\n\n Introduzca altura real");
+        scanf ("%f"; &altura);
+
+        area = (base * altura)/2;   // Formula matematica para calcular el area del triangulo.
+        printf ("\n\n El area del triangulo es: ", area);
+        scanf ("%f", &area);
+
+        return 0;
+
+}
+
+       
---
+++ volumen del trapecio.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{
+    /* Andrea Hernández 
+    Equipo
+    22 abr 21
+    Programa: Volumen de un trapecio
+    */
+}
+
+int main()
+{
+        float base mayor, base menor, altura, area;
+
+        printf ("\n\n Introduzca la base mayor");
+        scanf ("%f", &base mayor);
+        printf ("\n\n Introduzca la base menor");
+        scanf ("%f"; &base menor;ç
+        printf ("\n\n Introduzca la altura");
+        scanf ("%f", &altura);
+        printf ("\n\n Introduzca el area");
+        scanf ("%f"; &area;
+
+
+        volumen = (base mayor + base menor)/2 * altura;   // Formula matematica para calcular el volumen de un trapecio.
+        printf ("\n\n El volumen del trapecio es: ", volumen);
+        scanf ("%f", &volumen);
+
+        return 0;
+
+}
